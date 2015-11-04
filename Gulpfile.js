440aa11c9e1f990351446f28013b58f9fc8f71f6
Pablo-Rodriguez <pablorp1996@gmail.com>
2015-11-04 12:19:05 +0100

    Now the production version is called Vec2.min.js and it's in the lib/ directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
 	gulp.src('lib/Vec2.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('lib/uglify'));
+        .pipe(rename('Vec2.min.js'))
+		.pipe(gulp.dest('lib/'));
 });
 
 gulp.task('default', ['js']);
